b7066d9a19a3ef74ada3fa48d6c5dc97f4e9aadf
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2026-02-16 19:28:05 -0600

    Lab04-Process. - Part 3 implemented. Parent process communicates with child process.

--- 004Process/source/main.c
+++ 004Process/source/main.c
@@ -3,21 +3,53 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-int main(void) {
-  pid_t children_pid = fork();
+int child_process(int fd);
 
-  if (children_pid == -1) {
-    printf("Error creating children process. No process spawned. Terminating.\n");
-  } else if (children_pid) {
-    pid_t current_pid = getpid();
-    printf("Parent Process: PID = %d, Child created PID = %d\n", current_pid, children_pid);
-    printf("Parent Process: Waiting for child...\n");
-    waitpid(children_pid, NULL, 0);
-    printf("Parent Process: Child finished execution.\n");
+int main(void) {
+  int pipe_file_descriptor[2];
+  char message[20] = "I am your father.";
+
+  if (pipe(pipe_file_descriptor)) {
+    printf("Error creating the pipe. Terminating.\n");
   } else {
-    pid_t current_pid = getpid();
-    pid_t parent_pid = getppid();
-    printf("Child Process: PID = %d, My Parent PID = %d\n", current_pid, parent_pid);
-    sleep(1);
+    printf("Pipe created successfully!\n");
+
+    pid_t children_pid = fork();
+    
+    if (children_pid == -1) {
+      printf("Error creating children process. No process spawned. Terminating.\n");
+    } else if (children_pid) {
+      // it's the parent process
+      pid_t current_pid = getpid();
+      printf("Parent Process: PID = %d, Child created PID = %d\n", current_pid, children_pid);
+
+      printf("Parent Process: Writing '%s'\n", message);
+      write(pipe_file_descriptor[1], message, 20);
+
+      printf("Parent Process: Waiting for child...\n");
+      waitpid(children_pid, NULL, 0);
+      printf("Parent Process: Child finished execution.\n");
+    } else {
+      // it's the child process, calls function
+      child_process(pipe_file_descriptor[0]);
+    }
   }
+
+}
+
+/**
+ * A simple child process.
+ * @param fd: file descriptor for pipe unilateral communication
+ * @return execution status of the child process
+ */
+int child_process(int fd) {
+  char message[20];
+  
+  pid_t current_pid = getpid();
+  pid_t parent_pid = getppid();
+  printf("Child Process: PID = %d, my parent PID = %d\n", current_pid, parent_pid);
+  
+  read(fd, message, 20);
+  printf("Child Process: Received '%s'\n", message);
+  printf("Child Process: Nooooooooooooo\n");
 }
