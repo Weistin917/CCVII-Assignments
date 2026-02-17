c2edaf53fe59675ca106084c8601db5c41dd6c90
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2026-02-16 18:41:31 -0600

    Lab04-Process. - Part 2 implemented. Parent process waits for children to terminate using waitpid().

--- 004Process/source/main.c
+++ 004Process/source/main.c
@@ -1,17 +1,19 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 
 int main(void) {
   pid_t children_pid = fork();
 
   if (children_pid == -1) {
-    
+    printf("Error creating children process. No process spawned. Terminating.\n");
   } else if (children_pid) {
     pid_t current_pid = getpid();
-    printf("Parent Process: PID = %d\n", current_pid);
-    printf("Child created: PID = %d\n", children_pid);
-    sleep(1);
+    printf("Parent Process: PID = %d, Child created PID = %d\n", current_pid, children_pid);
+    printf("Parent Process: Waiting for child...\n");
+    waitpid(children_pid, NULL, 0);
+    printf("Parent Process: Child finished execution.\n");
   } else {
     pid_t current_pid = getpid();
     pid_t parent_pid = getppid();
