1b5d96378caf376cd375880fa7ca3a0ff715f512
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2026-02-18 18:32:31 -0600

    Lab04-Process. - Part 4 implemented. Multiple children process creation.

--- 004Process/source/main.c
+++ 004Process/source/main.c
@@ -3,53 +3,49 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-int child_process(int fd);
+#define   CHILD_PROCESSES   3
+
+int child_process(int child_id);
 
 int main(void) {
-  int pipe_file_descriptor[2];
-  char message[20] = "I am your father.";
+  int i;
+  pid_t child_ids[CHILD_PROCESSES];
+  pid_t child_pid, current_pid = getpid();
 
-  if (pipe(pipe_file_descriptor)) {
-    printf("Error creating the pipe. Terminating.\n");
-  } else {
-    printf("Pipe created successfully!\n");
+  printf("Parent Process: PID = %d\n", current_pid);
+  printf("Parent Process: Spawning children...\n");
 
-    pid_t children_pid = fork();
-    
-    if (children_pid == -1) {
+  for (i = 0; i < CHILD_PROCESSES; i++) {
+    child_pid = fork();
+
+    if (child_pid == -1) {
       printf("Error creating children process. No process spawned. Terminating.\n");
-    } else if (children_pid) {
-      // it's the parent process
-      pid_t current_pid = getpid();
-      printf("Parent Process: PID = %d, Child created PID = %d\n", current_pid, children_pid);
-
-      printf("Parent Process: Writing '%s'\n", message);
-      write(pipe_file_descriptor[1], message, 20);
-
-      printf("Parent Process: Waiting for child...\n");
-      waitpid(children_pid, NULL, 0);
-      printf("Parent Process: Child finished execution.\n");
+    } else if (!child_pid) {
+      // child_pid == 0, child process
+      child_process(i);
+      break;
     } else {
-      // it's the child process, calls function
-      child_process(pipe_file_descriptor[0]);
+      // parent process, store the child pid
+      child_ids[i] = child_pid;
     }
   }
 
+  if (child_pid) {
+    for (i = 0; i < CHILD_PROCESSES; i++) {
+      // parent process wait for the children
+      waitpid(child_ids[i], NULL, 0);
+    }
+    printf("Parent Process: Children finished execution.\n");
+  }
 }
 
 /**
  * A simple child process.
- * @param fd: file descriptor for pipe unilateral communication
+ * @param child_id: child id number relative to the parent
  * @return execution status of the child process
  */
-int child_process(int fd) {
-  char message[20];
-  
+int child_process(int child_id) {
   pid_t current_pid = getpid();
   pid_t parent_pid = getppid();
-  printf("Child Process: PID = %d, my parent PID = %d\n", current_pid, parent_pid);
-  
-  read(fd, message, 20);
-  printf("Child Process: Received '%s'\n", message);
-  printf("Child Process: Nooooooooooooo\n");
+  printf("Child %d: PID = %d, my parent PID = %d\n", child_id, current_pid, parent_pid);
 }
